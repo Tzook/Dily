5d723039fa16d963570c9b1044788239bf464502
Noam <noam@mail.com>
2016-03-05 17:37:11 +0200

    added lobby game

--- app/lib/lobby/lobby.component.js
+++ app/lib/lobby/lobby.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', '../dice/die.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1;
+    var core_1, router_1, die_component_1;
     var LobbyComponent;
     return {
         setters:[
@@ -19,20 +19,29 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (die_component_1_1) {
+                die_component_1 = die_component_1_1;
             }],
         execute: function() {
             LobbyComponent = (function () {
                 function LobbyComponent(_router) {
                     this._router = _router;
+                    this._result = 3;
+                    this._roll = 0;
                 }
                 LobbyComponent.prototype.joinRoom = function (room) {
                     this._router.navigate(['Room', { room: room }]);
                 };
+                LobbyComponent.prototype.changeResult = function () {
+                    this._result = Math.floor(Math.random() * 6) + 1;
+                    this._roll++;
+                };
                 LobbyComponent = __decorate([
                     core_1.Component({
                         selector: 'lobby',
-                        template: "\n        <h1>~Dily~</h1>\n        <button (click)=\"joinRoom(0)\">Create room</button>\n        <p>OR</p>\n        <input #room type=\"number\" placeholder=\"Room\" max=\"100000\" min=\"1\" required>\n        <button (click)=\"joinRoom(room.value)\">Join room</button>\n    ",
-                        directives: [],
+                        template: "\n        <h1>~Dily~</h1>\n        <div class=\"lobby-die\">\n            <die scale=\"1.0\" [result]=\"_result\" [roll]=\"_roll\" (click)=\"changeResult()\"></die>\n        </div>\n        <button (click)=\"joinRoom(0)\">Create room</button>\n        <p>OR</p>\n        <input #room type=\"number\" placeholder=\"Room\" max=\"100000\" min=\"1\" required>\n        <button (click)=\"joinRoom(room.value)\">Join room</button>\n    ",
+                        directives: [die_component_1.DieComponent],
                         providers: [],
                     }), 
                     __metadata('design:paramtypes', [router_1.Router])
--- app/lib/lobby/lobby.component.ts
+++ app/lib/lobby/lobby.component.ts
@@ -1,22 +1,36 @@
 import {Component} from 'angular2/core';
 import {Router} from 'angular2/router';
+import {DieComponent} from '../dice/die.component';
 
 @Component({
     selector: 'lobby',
     template: `
         <h1>~Dily~</h1>
+        <div class="lobby-die">
+            <die scale="1.0" [result]="_result" [roll]="_roll" (click)="changeResult()"></die>
+        </div>
         <button (click)="joinRoom(0)">Create room</button>
         <p>OR</p>
         <input #room type="number" placeholder="Room" max="100000" min="1" required>
         <button (click)="joinRoom(room.value)">Join room</button>
     `,
-    directives: [],
+    directives: [DieComponent],
     providers: [],
 })
 export class LobbyComponent {
-    constructor(private _router:Router) {}
+    private _result:number;
+    private _roll:number;
+    constructor(private _router:Router) {
+        this._result = 3;
+        this._roll = 0;
+    }
     
     joinRoom(room:string) {
         this._router.navigate(['Room', {room}]);
+    }
+    
+    changeResult() {
+        this._result = Math.floor(Math.random() * 6) + 1;
+        this._roll++;
     }
 }
